85f90d6c08f62df66ad34829a67bda64b6356ae2
Siwei Han <lillian423@126.com>
2024-04-19 11:07:14 +0800

    feat: add new, filled, fill, throw, goto, switch

--- experiment_2/src/lian/lang/parser/smali_parser.py
+++ experiment_2/src/lian/lang/parser/smali_parser.py
@@ -3,6 +3,7 @@
 from . import common_parser
 import re
 
+type_list = ['variable', 'parameter', 'number', 'string', 'float', 'character', 'identifier', 'list', 'body', 'class_identifier']
 
 class Parser(common_parser.Parser):
     def is_comment(self, node):
@@ -15,15 +16,8 @@ class Parser(common_parser.Parser):
 
     def obtain_literal_handler(self, node):
         LITERAL_MAP = {
-            "number":self.regular_number_literal,
-            "float" : self.regular_number_literal,
-            "NaN":self.regular_literal,
-            "Infinity":self.regular_literal,
-            "string":self.string_literal,
-            "boolean":self.regular_literal,
-            "character":self.character_literal,
-            "null":self.regular_literal
         }
+
         return LITERAL_MAP.get(node.type, None)
 
     def is_literal(self, node):
@@ -32,34 +26,6 @@ class Parser(common_parser.Parser):
     def literal(self, node, statements, replacement):
         handler = self.obtain_literal_handler(node)
         return handler(node, statements, replacement)
-    
-    def string_literal(self, node, statements, replacement):
-        replacement = []
-        for child in node.named_children:
-            self.parse(child, statements, replacement)
-
-        ret = self.read_node_text(node)
-        if replacement:
-            for r in replacement:
-                (expr, value) = r
-                ret = ret.replace(self.read_node_text(expr), value)
-
-        ret = self.handle_hex_string(ret)
-
-        return self.escape_string(ret)
-
-    def character_literal(self, node, statements, replacement):
-        value = self.read_node_text(node)
-        return "'%s'" % value
-    
-    def regular_number_literal(self, node, statements, replacement):
-        value = self.read_node_text(node)
-        #value=re.compile(r'[LlSsTtf]').sub("",value)
-        value = self.common_eval(value)
-        return self.read_node_text(node)
-
-    def regular_literal(self, node, statements, replacement):
-        return self.read_node_text(node)
 
     def check_declaration_handler(self, node):
         DECLARATION_HANDLER_MAP = {
@@ -104,22 +70,25 @@ class Parser(common_parser.Parser):
         opcode = self.find_child_by_type(node, "opcode")
         shadow_opcode = self.read_node_text(opcode)
         print(shadow_opcode)
-        variables = self.find_children_by_type(node, "variable")
-        parameters = self.find_children_by_type(node, "parameter")
-        values = variables + parameters
-        print(values)
-        # return
-        # values = self.find_children_by_type(node, "variable")
+        values = {}
+        for type in type_list:
+            values[type] = []
+        for type in type_list:
+            values[type].extend(self.find_children_by_type(node, type))
         if shadow_opcode == "nop":
             return ''
         elif re.compile(r'^move.*').match(shadow_opcode):
-            if "result" in shadow_opcode or "exception" in shadow_opcode:
-                v0 = self.read_node_text(values[0])
+            if "result" in shadow_opcode in shadow_opcode:
+                v0 = self.read_node_text(values["variable"][0])
                 tmp_var = self.tmp_variable(statements)
-                statements.append({"assign_stmt": {"target": v0, "operand": tmp_var}})
+                statements.append({"assign_stmt": {"target": v0, "operand": tmp_var, "operator": "result"}})
+            elif "exception" in shadow_opcode:
+                v0 = self.read_node_text(values["variable"][0])
+                tmp_var = self.tmp_variable(statements)
+                statements.append({"assign_stmt": {"target": v0, "operand": tmp_var, "operator": "exception"}})
             else:
-                v0 = self.read_node_text(values[0])
-                v1 = self.read_node_text(values[1])
+                v0 = self.read_node_text(values["variable"][0])
+                v1 = self.read_node_text(values["variable"][1])
                 statements.append({"assign_stmt": {"target": v0, "operand": v1}})
             return v0
         elif re.compile(r'^return.*').match(shadow_opcode):
@@ -127,79 +96,100 @@ class Parser(common_parser.Parser):
                 statements.append({"return_stmt": {"target": ''}})
                 return ''
             else:
-                shadow_name = ""
-                name = values[0]
-                shadow_name = self.parse(name, statements)
-                statements.append({"return_stmt": {"target": shadow_name}})
-                return shadow_name
+                v0 = self.read_node_text(values["variable"][0])
+                statements.append({"return_stmt": {"target": v0}})
+                return v0
         elif re.compile(r'^const.*').match(shadow_opcode):
-            print(values[0])
-            print(self.read_node_text(values[0]))
-            v0 = self.read_node_text(values[0])
-            node = values[1]
-            if self.find_child_by_type(node, 'literal'):
-                v1 = self.read_node_text(self.find_child_by_type(node, 'literal').namedchildren[0])
-            elif self.find_child_by_type(node, 'identifier'):
-                v1 = self.read_node_text(self.find_child_by_type(node, 'identifier'))
-            statements.append({"assign_stmt": {"target": v0, "operand": v1}})
+            v0 = self.read_node_text(values["variable"][0])
+            if 'string' in shadow_opcode:
+                string = self.find_child_by_type(values["string"][0], 'string_fragment')
+                shadow_string = self.read_node_text(string)
+                print(shadow_string)
+                statements.append({"assign_stmt": {"target": v0, "operand": shadow_string}})
+            elif 'class' in shadow_opcode: 
+                pass
+            else:
+                number = values["number"][0]
+                shadow_number = self.read_node_text(number)
+                statements.append({"assign_stmt": {"target": v0, "operand": shadow_number}})
             return v0
         elif re.compile(r'^check.*').match(shadow_opcode):
-            pass
+            v0 = self.read_node_text(values["variable"][0])
+            class_identifier = values['class_identifier'][0]
+            shadow_class = self.read_node_text(class_identifier)
+            statements.append({"assign_stmt": {"target": v0, "operand": shadow_class, "operator": "check-cast"}})
+            return v0
         elif shadow_opcode == "instance-of":
-            v1 = self.read_node_text(values[1])
+            v1 = self.read_node_text(values["variable"][1])
             v2 = self.parse(values[2], statements)
             tmp_var = self.tmp_variable(statements)
             statements.append({"assign_stmt":
                                    {"target": tmp_var, "operator": "instanceof", "operand": v1,
                                     "operand2": v2}})
-            v0 = self.read_node_text(values[0])
+            v0 = self.read_node_text(values["variable"][0])
             statements.append({"assign_stmt": {"target": v0, "operand": tmp_var}})
             return v0
         elif shadow_opcode == 'array-length':
-            v0 = self.read_node_text(values[0])
-            v1 = self.read_node_text(values[1])
+            v0 = self.read_node_text(values["variable"][0])
+            v1 = self.read_node_text(values["variable"][1])
             statements.append({"assign_stmt": {"target": v0, "operand": v1, "operator": "array-length"}})
-            # array-length v0, v1 将 v1 中数组的长度计算出来，然后将这个长度值存储到 v0 中
-            pass
+            return v0
         elif re.compile(r'^new.*').match(shadow_opcode):
             # new-instance vA, type-id 创建一个类的新实例（对象）, type-id: 是该类的类型标识符，指定要创建哪个类的实例
             # new-array v0, v1, type-id 声明数组并为其分配内存和类型, 创建一个数组，其大小由寄存器v1的值决定。数组的引用将会被存储在v0寄存器中。
             if shadow_opcode == "new-instance":
                 glang_node = {}
-                mytype = self.parse(values[1], statements)
+                mytype = self.read_node_text(values['class_identifier'][0])
                 glang_node["data_type"] = mytype
                 tmp_var = self.tmp_variable(statements)
                 glang_node["target"] = tmp_var
                 statements.append({"new_instance": glang_node})
-                v0 = self.read_node_text(values[0])
+                v0 = self.read_node_text(values["variable"][0])
                 statements.append({"assign_stmt": {"target": v0, "operand": tmp_var}})
                 return v0
             else:
-                type = self.read_node_text(self.find_child_by_type(values[2], "primitives"))
-                v1 = self.read_node_text(values[0])
+                type = self.read_node_text(self.find_child_by_type_type(node, "array_type", "primitive_type"))
+                v0 = self.read_node_text(values["variable"][0])
+                v1 = self.read_node_text(values["variable"][1])
                 tmp_var = self.tmp_variable(statements)
-                statements.append({"new_array": {"type": type, "target": tmp_var}})
-                return tmp_var
+                statements.append({"new_array": {"type": type, "attr": v1, "target": tmp_var}})
+                statements.append({"assign_stmt": {"target": v0, "operand": tmp_var}})
+                return v0
         elif re.compile(r'^filled.*').match(shadow_opcode):
             # filled-new-array创建一个新的数组，并立即用给定的值填充它
             # filled-new-array {v0, v1, v2}, [I 这里，[I表示一个int类型的数组，v0, v1, v2是将被放入新数组的值所在的寄存器。数组创建和填充之后，其引用会被放置在某个寄存器中
             # filled-new-array/range {v0 .. v5}, [I 使用了v0到v5寄存器中的值来填充一个新的int数组
-            pass
+            if "range" in shadow_opcode:
+                type = self.read_node_text(self.find_child_by_type_type(node, "array_type", "primitive_type"))
+                range_node = self.read_node_text(self.find_child_by_type(node,"range"))
+                tmp_var = self.tmp_variable(statements)
+                statements.append({"new_array": {"type": type, "target": tmp_var}})
+                statements.append({"assign_stmt": {"target": range_node, "operand": tmp_var, "operator": "filled-new-array/range"}})
+                return range_node
+            else:
+                pass
+                type = self.read_node_text(self.find_child_by_type_type(node, "array_type", "primitive_type"))
+                v0 = self.read_node_text(values["list"][0])
+                tmp_var = self.tmp_variable(statements)
+                statements.append({"new_array": {"type": type, "target": tmp_var}})
+                statements.append({"assign_stmt": {"target": v0, "operand": tmp_var, "operator": "filled-new-array"}})
+                return v0
         elif re.compile(r'^fill-.*').match(shadow_opcode):
             # fill-array-data v0, :array_data 将预定义的数据集合填充到之前声明的数组中
-            pass
+            v0 = self.read_node_text(values["variable"][0])
+            label = self.read_node_text(self.find_child_by_type(node, "label"))
+            statements.append({"assign_stmt": {"target": v0, "operand": label, "operator": "fill-array-data"}})
+            return v0
         elif re.compile(r'^throw.*').match(shadow_opcode):
-            shadow_expr = ""
-            expr = values[0]
-            shadow_expr = self.parse(expr, statements)
+            shadow_expr = self.read_node_text(values["variable"][0])
             statements.append({"throw_stmt": {"target": shadow_expr}})
-            pass
+            return
         elif re.compile(r'^goto.*').match(shadow_opcode):
             # 跳转到同一方法内的指定位置继续执行代码
             # goto :label_start :label_start 是一个标签，可以在代码中跳转到它标记的位置。
-            v0 = self.parse(values[0], statements)
-            statements.append({"goto": {"target": v0}})
-            pass
+            label = self.read_node_text(self.find_child_by_type(node, "label"))
+            statements.append({"goto": {"target": label}})
+            return
         elif re.search(r'-switch$', shadow_opcode):
             # packed switch语句的case标签密集排布时使用
             # sparse switch语句中的case标签稀疏分布时使用
@@ -222,15 +212,7 @@ class Parser(common_parser.Parser):
         elif re.compile(r'^aput.*').match(shadow_opcode):
             pass
         
-        elif re.compile(r'^invoke.*').match(shadow_opcode):
-            pass
 
-        elif re.compile(r'^rsub.*').match(shadow_opcode):
-            dest = self.parse(node.named_children[1], statements)
-            source1 = self.parse(node.named_children[2], statements)
-            source2 = self.parse(node.named_children[3], statements)
-            statements.append({"assign_stmt": {"target": dest, "operator": '-', "operand": source2,"operand2": source2}})
-            return dest
         elif re.compile(r'^add.*/2addr').match(shadow_opcode):
             return self.binary_expression_2addr(node, statements, "+")
         elif re.compile(r'^sub.*/2addr').match(shadow_opcode):
@@ -287,36 +269,7 @@ class Parser(common_parser.Parser):
                                'double-to-float','int-to-byte','int-to-char','int-to-short']:
             return self.unary_expression(node, statements, 'cast')
         
-        elif re.compile(r'^iput.*').match(shadow_opcode): 
-            source = self.parse(node.named_children[1], statements)
-            receiver_object = self.parse(node.named_children[2], statements)
-            field=self.find_child_by_type(node.named_children[3],"field_identifier")
-            shadow_field=self.read_node_text(field)
-            statements.append({"field_write": {"receiver_object": receiver_object, "field": shadow_field, "source": source}})
-        elif re.compile(r'^iget.*').match(shadow_opcode):
-            target = self.parse(node.named_children[1], statements)
-            receiver_object = self.parse(node.named_children[2], statements)
-            field=self.find_child_by_type(node.named_children[3],"field_identifier")
-            shadow_field=self.read_node_text(field)
-            statements.append({"field_read": {"target": target, "receiver_object": receiver_object, "field": shadow_field}})
-        elif re.compile(r'^sget.*').match(shadow_opcode):
-            target = self.parse(node.named_children[1], statements)
-            source = node.named_children[2]
-            receiver_object = self.find_child_by_type(source,"class_identifier")
-            shadow_receiver_object=self.read_node_text(receiver_object)
-            field = self.find_child_by_type(source,"field_identifier")
-            shadow_field = self.read_node_text(field)
-            statements.append({"field_read": {"target": target, "receiver_object": shadow_receiver_object, "field": shadow_field}})
-        elif re.compile(r'^sput.*').match(shadow_opcode):
-            source = self.parse(node.named_children[1], statements)
-            target = node.named_children[2]
-            receiver_object = self.find_child_by_type(target,"class_identifier")
-            shadow_receiver_object=self.read_node_text(receiver_object)
-            field = self.find_child_by_type(target,"field_identifier")
-            shadow_field = self.read_node_text(field)
-            statements.append({"field_write": {"receiver_object": shadow_receiver_object, "field": shadow_field, "source": source}})
 
-    
     def unary_expression(self, node, statements,op):
         dest = self.parse(node.named_children[1], statements)
         source = self.parse(node.named_children[2], statements)
